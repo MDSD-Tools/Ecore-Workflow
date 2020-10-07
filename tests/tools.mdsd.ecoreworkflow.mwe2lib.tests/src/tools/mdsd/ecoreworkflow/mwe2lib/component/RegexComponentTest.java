569e6b3fd8fbb1eca07261ea024c91b48d535b26
Sebastian Krach <krach@fzi.de>
2020-10-07 11:08:33 +0200

    Fixed formatting

--- tests/tools.mdsd.ecoreworkflow.mwe2lib.tests/src/tools/mdsd/ecoreworkflow/mwe2lib/component/RegexComponentTest.java
+++ tests/tools.mdsd.ecoreworkflow.mwe2lib.tests/src/tools/mdsd/ecoreworkflow/mwe2lib/component/RegexComponentTest.java
@@ -23,62 +23,69 @@ import tools.mdsd.library.standalone.initialization.StandaloneInitializationExce
 import tools.mdsd.library.standalone.initialization.StandaloneInitializerBuilder;
 
 class RegexComponentTest {
-	static final String PROJECT_NAME = "some.test.project";
-	static Path TEMP_FOLDER;
-	static Path TEMP_FILE;
-	
-	@BeforeAll
-	static void setUp() throws StandaloneInitializationException, IOException {
-		TEMP_FOLDER = Files.createTempDirectory("test_");
-		TEMP_FILE = Files.createTempFile(TEMP_FOLDER, "some", "file");
-		
-		StandaloneInitializerBuilder.builder()
-			.useEcoreClasspathDetection(true)
-			.addCustomTask(new InitializationTask() {
-				@Override
-				public void initilizationWithoutPlatform() throws StandaloneInitializationException {
-					EcorePlugin.getPlatformResourceMap()
-	            		.put(PROJECT_NAME, URI.createURI(TEMP_FOLDER.toUri().toString()));
-				}
-				
-				@Override
-				public void initializationWithPlatform() throws StandaloneInitializationException {
-					var progMon = new NullProgressMonitor();
-					var project = ResourcesPlugin.getWorkspace().getRoot().getProject(PROJECT_NAME);
-					try {	
-						if (project.exists()) project.delete(true, progMon);
-						project.create(progMon);
-						project.open(progMon);
-						var testFile = project.getFile(TEMP_FILE.getFileName().toString());
-						testFile.createLink(TEMP_FILE.toUri(), IResource.REPLACE, progMon);
-					} catch (CoreException e) {
-						throw new RuntimeException(e);
-					}
-				}
-			})
-			.build().init();
-	}
-	
-	@Test
-	void testFromFilename() throws Exception {
-		var cut = new RegexComponent();
-		
-		var converted = cut.fromFilename("test");
-		assertFalse(converted.isAbsolute());
-		assertEquals(Paths.get("test"), converted);
-		
-		var resourceUri = "platform:/resource/" + PROJECT_NAME + "/" + TEMP_FILE.getFileName().toString();
-		converted = cut.fromFilename(resourceUri);
-		assertTrue(converted.isAbsolute());
-		assertEquals(TEMP_FILE, converted);
-		
-		converted = cut.fromFilename(TEMP_FOLDER.toString());
-		assertTrue(converted.isAbsolute());
-		assertEquals(TEMP_FOLDER, converted);
-		
-		var uri = TEMP_FOLDER.toUri();
-		converted = cut.fromFilename(uri.toString());
-		assertEquals(Paths.get(uri), converted);
-	}
+    static final String PROJECT_NAME = "some.test.project";
+    static Path TEMP_FOLDER;
+    static Path TEMP_FILE;
+
+    @BeforeAll
+    static void setUp() throws StandaloneInitializationException, IOException {
+        TEMP_FOLDER = Files.createTempDirectory("test_");
+        TEMP_FILE = Files.createTempFile(TEMP_FOLDER, "some", "file");
+
+        StandaloneInitializerBuilder.builder()
+            .useEcoreClasspathDetection(true)
+            .addCustomTask(new InitializationTask() {
+                @Override
+                public void initilizationWithoutPlatform() throws StandaloneInitializationException {
+                    EcorePlugin.getPlatformResourceMap()
+                        .put(PROJECT_NAME, URI.createURI(TEMP_FOLDER.toUri()
+                            .toString()));
+                }
+
+                @Override
+                public void initializationWithPlatform() throws StandaloneInitializationException {
+                    var progMon = new NullProgressMonitor();
+                    var project = ResourcesPlugin.getWorkspace()
+                        .getRoot()
+                        .getProject(PROJECT_NAME);
+                    try {
+                        if (project.exists())
+                            project.delete(true, progMon);
+                        project.create(progMon);
+                        project.open(progMon);
+                        var testFile = project.getFile(TEMP_FILE.getFileName()
+                            .toString());
+                        testFile.createLink(TEMP_FILE.toUri(), IResource.REPLACE, progMon);
+                    } catch (CoreException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            })
+            .build()
+            .init();
+    }
+
+    @Test
+    void testFromFilename() throws Exception {
+        var cut = new RegexComponent();
+
+        var converted = cut.fromFilename("test");
+        assertFalse(converted.isAbsolute());
+        assertEquals(Paths.get("test"), converted);
+
+        var resourceUri = "platform:/resource/" + PROJECT_NAME + "/" + TEMP_FILE.getFileName()
+            .toString();
+        converted = cut.fromFilename(resourceUri);
+        assertTrue(converted.isAbsolute());
+        assertEquals(TEMP_FILE, converted);
+
+        converted = cut.fromFilename(TEMP_FOLDER.toString());
+        assertTrue(converted.isAbsolute());
+        assertEquals(TEMP_FOLDER, converted);
+
+        var uri = TEMP_FOLDER.toUri();
+        converted = cut.fromFilename(uri.toString());
+        assertEquals(Paths.get(uri), converted);
+    }
 
 }
